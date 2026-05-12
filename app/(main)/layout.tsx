709d8e5f2601655f899d80d5a62ba5c3f89422d8
dsabirbaev <davran9559@gmail.com>
2026-05-12 15:59:37 +0500

    fixed footer height

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,10 +7,10 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <main className="flex-1">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
